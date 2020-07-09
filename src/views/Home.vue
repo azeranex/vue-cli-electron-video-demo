8127ccdcaaa6362f00d04579a231d67fa3a190f5
Zhongkit R <azeranex@hotmail.com>
2020-07-09 08:09:49 +0700

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <video width="320" height="240" controls>
+      <source src="Holy - 35006.mp4" type="video/mp4">
+    </video>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
